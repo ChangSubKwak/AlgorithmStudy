48d24ec162b04b9f3981307f39be64c7052e968f
unknown <paradise1732@gmail.com>
2020-06-11 03:10:53 +0900

    Commit 20200611 #1

---
+++ first/SelectionSort.java
@@ -0,0 +1,28 @@
+package first;
+
+// 제일 작은 것을 앞으로 보내보자
+// Time Complexity : n^2
+
+public class SelectionSort {
+	public static void main(String[] args) {
+    int min, minIdx = 0, temp;
+		int[] array = { 2, 5, 9, 4, 1 };
+
+    for (int i = 0 ; i < array.length ; i++ ) {
+      min = 100;
+      for (int j = i ; j < array.length ; j++ ) {
+        if (min > array[j]) {
+          min = array[j];
+          minIdx = j;
+        }
+      }
+      temp = array[minIdx];
+      array[minIdx] = array[i];
+      array[i] = temp;
+    }
+		for (int i = 0 ; i < array.length ; i++)
+			System.out.print(array[i] + " ");
+		System.out.println();
+	}
+}
+
---
+++ first/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+javac $1.java
+cd ../
+java algo.$1
