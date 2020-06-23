4844e9b24abb24420fafc3e8998f40a5b50133dd
changsubkwak <changsubkwak@100doit.com>
2020-06-24 01:07:18 +0900

    Commit 20200624 #1

---
+++ first/UnionFind.java
@@ -0,0 +1,11 @@
+package first;
+
+public class UnionFind {
+	public void unionParent(int[] data, int node1, int node2) {
+	}
+	public static void main(String[] args) {
+		int[] data = { 1, 2, 3, 4, 5, 6, 7, 8 }; 
+		//System.out.println("TEST");
+	}
+}
+
